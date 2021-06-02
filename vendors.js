dcfb515eb69841457dd8b42b5b1a20dca4e7a630
Musab-Al-omari <musabsomari@gmail.com>
2021-06-02 17:44:04 +0300

    gg

---
+++ vendors.js
@@ -0,0 +1,54 @@
+const AWS = require('aws-sdk');
+const uuid = require('uuid').v4;
+const faker = require('faker')
+
+AWS.config.update({ region: 'us-east-1' });
+
+const sns = new AWS.SNS();
+
+const topic = 'arn:aws:sns:us-east-1:607350834221:pickup';
+
+
+setInterval(() => {
+  const message = {
+    orderId: uuid(),
+    customer: faker.name.findName(),
+    vendorId: 'arn:aws:sns:us-east-1:607350834221:vendor'
+  }
+
+  const payload = {
+    Message: JSON.stringify(message),
+    TopicArn: topic,
+  };
+
+  sns.publish(payload).promise()
+    .then(data => {
+      console.log(data);
+    })
+    .catch(console.error);
+}, 5000);
+
+///////////////////////////////////////////////
+
+// TODO: Poll from sqs
+
+const { Consumer } = require('sqs-consumer');
+
+const app = Consumer.create({
+  queueUrl: 'https://sqs.us-east-1.amazonaws.com/607350834221/vender',
+  handleMessage: handler,
+});
+
+function handler(message) {
+  console.log(message.Body);
+}
+
+app.on('error', (err) => {
+  console.error(err.message);
+});
+
+app.on('processing_error', (err) => {
+  console.error(err.message);
+});
+
+app.start();
